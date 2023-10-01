cf3c591d14117e49be14cc81d8b7cffd467d160f
Jan Nowak <nowak.jan@yahoo.com>
2023-10-01 13:41:47 +0200

    fix validateForm function

--- src/helpers/validateForm.js
+++ src/helpers/validateForm.js
@@ -1,10 +1,10 @@
-export default function validateForm(form){
-    const errors = [];
-    if(form.continent === "Europa" && form.surname.length < 2){
-        errors.push("ErrorContinent")
-    }
-    else if(form.name === ""){
-        errors.push("ErrorName")
-    }
-    return errors;
+export default function validateForm(form) {
+  const errors = [];
+  if (form.continent === "Europa" && form.surname.length < 2) {
+    errors.push("ErrorContinent");
+  }
+  if (form.name === "") {
+    errors.push("ErrorName");
+  }
+  return errors;
 }
